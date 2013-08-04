c88ffa332af02e078483c195ca06f81ab02fd86b
Eric Glassman <eglassman@scitent.com>
2013-08-04 18:38:29 -0400

    runs compass scaffold command to add baseline file structure

---
+++ report/report_parser/assets/config.rb
@@ -0,0 +1,73 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "stylesheets"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "javascripts"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+
+
+###########################
+# Compass Scaffold Output #
+###########################
+
+# `compass create`
+
+#*********************************************************************
+#Congratulations! Your compass project has been created.
+#
+#directory sass/
+#  create config.rb
+#  create sass/screen.scss
+#  create sass/print.scss
+#  create sass/ie.scss
+#  create stylesheets/ie.css
+#  create stylesheets/screen.css
+#  create stylesheets/print.css
+#
+#You may now add and edit sass stylesheets in the sass subdirectory of your project.
+#
+#                                                                                                                                  Sass files beginning with an underscore are called partials and won't be
+#compiled to CSS, but they can be imported into other sass stylesheets.
+#
+#You can configure your project by editing the config.rb configuration file.
+#
+#You must compile your sass stylesheets into CSS when they change.
+#This can be done in one of the following ways:
+#  1. To compile on demand:
+#     compass compile [path/to/project]
+#  2. To monitor your project for changes and automatically recompile:
+#     compass watch [path/to/project]
+#
+#More Resources:
+#  * Website: http://compass-style.org/
+#  * Sass: http://sass-lang.com
+#  * Community: http://groups.google.com/group/compass-users/
+#
+#
+#To import your new stylesheets add the following lines of HTML (or equivalent) to your webpage:
+#<head>
+#  <link href="/stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
+#  <link href="/stylesheets/print.css" media="print" rel="stylesheet" type="text/css" />
+#  <!--[if IE]>
+#      <link href="/stylesheets/ie.css" media="screen, projection" rel="stylesheet" type="text/css" />
+#  <![endif]-->
+#</head>
+
